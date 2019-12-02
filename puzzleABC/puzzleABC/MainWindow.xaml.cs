f1be1e659da04b785d8e6658b23b2a659454838a
Vũ Phạm Đức Thắng <1712763@student.hcmus.edu.vn>
2019-12-03 00:58:13 +0700

    + snap không đúng vị trí + shuffe

--- puzzleABC/puzzleABC/MainWindow.xaml.cs
+++ puzzleABC/puzzleABC/MainWindow.xaml.cs
@@ -33,9 +33,11 @@ namespace puzzleABC
         const int mRows = 3;
         const int mCols = 3;
         CroppedBitmap[] objImg;
+        int[] map;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             objImg = new CroppedBitmap[9];
+            map = new int[mRows * mCols];
             var screen = new OpenFileDialog();
 
             if (screen.ShowDialog() == true)
@@ -52,6 +54,7 @@ namespace puzzleABC
                 CutImage(source);
                 SetPiecesPosition();
                 DrawPuzzleBoard();
+                Shuffle();
             }
 
         }
@@ -61,15 +64,15 @@ namespace puzzleABC
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (!((i == 2) && (j == 2)))
+                    if (!((i == mRows - 1) && (j == mCols - 1)))
                     {
                         height = (int)source.Height / 3;
                         width = (int)source.Width / 3;
                         //Debug.WrbiteLine($"Len = {len}");
                         var rect = new Int32Rect(j * width, i * height, width, height);
-                        var cropBitmap = new CroppedBitmap(source,
-                            rect);
+                        var cropBitmap = new CroppedBitmap(source, rect);
                         objImg[3 * i + j] = cropBitmap;
+                        map[3 * i + j] = (3 * i + j) % (mRows * mCols);
                     }
                 }
             }
@@ -106,44 +109,94 @@ namespace puzzleABC
 
         private void SetPiecesPosition()
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < mRows; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < mCols; j++)
                 {
-                    if (!((i == 2) && (j == 2)))
+                    
+                    if (!((i == mRows - 1) && (j == mCols - 1)))
                     {
                         var cropImage = new Image();
                         cropImage.Stretch = Stretch.Fill;
                         cropImage.Width = width;
                         cropImage.Height = height;
-                        cropImage.Source = objImg[3 * i + j];
-                        myCanvas.Children.Add(cropImage);
-                        Canvas.SetLeft(cropImage, startX + j * width);
-                        Canvas.SetTop(cropImage, startY + i * height);
+                        cropImage.Source = objImg[mRows * i + j];
 
-                        cropImage.MouseLeftButtonDown += beginDrag;
-                        cropImage.PreviewMouseLeftButtonUp += endDrag;
                     }
                 }
             }
         }
+
+        private void Shuffle()
+        {
+            Random r = new Random();
+            int n = mRows * mCols - 1;
+            for (int i = 0; i < 50; i++)
+            {
+                int i1 = 0, i2 = 0;
+                while (i1 == i2)
+                {
+                    i1 = r.Next(1, n);
+                    i2 = r.Next(1, n);
+                }
+
+                int temp = map[i1];
+                map[i1] = map[i2];
+                map[i2] = temp;
+            }
+
+            for (int i = 0; i < mRows; i++)
+            {
+                for (int j = 0; j < mCols; j++)
+                    if (!((i == mRows - 1) && (j == mCols - 1)))
+                    {
+                        var cropImage = new Image();
+                        cropImage.Stretch = Stretch.Fill;
+                        cropImage.Width = width;
+                        cropImage.Height = height;
+                        cropImage.Source = objImg[map[3 * i + j]];
+                        myCanvas.Children.Add(cropImage);
+                        Canvas.SetLeft(cropImage, startX + j * width);
+                        Canvas.SetTop(cropImage, startY + i * height);
+                        cropImage.MouseLeftButtonDown += beginDrag;
+                        cropImage.PreviewMouseLeftButtonUp += endDrag;
+                        cropImage.Tag = 3 * i + j;
+                    }
+            }
+
+        }
+
         Tuple<int, int> lastCell;
+        bool gameOver = false;
+
+        private void endDrag2(object sender, MouseEventArgs e)
+        {
+
+            Canvas.SetLeft(_selectedCropImage, startX + lastCell.Item1 * width);
+            Canvas.SetTop(_selectedCropImage, startY + lastCell.Item2 * height);
+
+            isDragging = false;
+        }
+
         private void endDrag(object sender, MouseButtonEventArgs e)
         {
-            isDragging = false;
             var position = e.GetPosition(this);
             var cellX = (int)(position.X - startX) / width;
             var cellY = (int)(position.Y - startY) / height;
-
-            if (cellX < mCols && cellY < mRows)
+            if (cellX < mCols && cellY < mRows && map[3 * cellX + cellY] == 0 && isDragging == true)
             {
                 Canvas.SetLeft(_selectedCropImage, startX + cellX * width);
                 Canvas.SetTop(_selectedCropImage, startY + cellY * height);
-            } else
+                map[3 * cellX + cellY] = map[3 * lastCell.Item1 + lastCell.Item2];
+                map[3 * lastCell.Item1 + lastCell.Item2] = 0;
+            }
+            else
             {
                 Canvas.SetLeft(_selectedCropImage, startX + lastCell.Item1 * width);
                 Canvas.SetTop(_selectedCropImage, startY + lastCell.Item2 * height);
             }
+            isDragging = false;
+
 
         }
 
@@ -151,14 +204,17 @@ namespace puzzleABC
 
         Image _selectedCropImage = null;
         Point _lastPosition;
-       
+
         private void beginDrag(object sender, MouseButtonEventArgs e)
         {
             var position = e.GetPosition(this);
             var cellX = (int)(position.X - startX) / width;
             var cellY = (int)(position.Y - startY) / height;
-            lastCell = new Tuple<int, int>(cellX, cellY);
-
+            if (cellX < mCols && cellY < mRows)
+            {
+                lastCell = new Tuple<int, int>(cellX, cellY);
+            }
+            // this.CaptureMouse();
             _selectedCropImage = sender as Image;
             _lastPosition = e.GetPosition(this);
             isDragging = true;
@@ -171,15 +227,24 @@ namespace puzzleABC
                 var dx = position.X - _lastPosition.X;
                 var dy = position.Y - _lastPosition.Y;
 
-                var lastLeft = Canvas.GetLeft(_selectedCropImage);
-                var lastTop = Canvas.GetTop(_selectedCropImage); 
-                Canvas.SetLeft(_selectedCropImage, lastLeft + dx);
-                Canvas.SetTop(_selectedCropImage, lastTop + dy);
-
-                _lastPosition = position;
-
+                if (position.X < startX - 20 || position.Y < startY - 10 || position.X > startX + (mRows * height) + 2 * height || position.Y > startY + (mCols * width) + 2 * width)
+                {
+                    isDragging = false;
+                    Canvas.SetLeft(_selectedCropImage, startX + lastCell.Item1 * width);
+                    Canvas.SetTop(_selectedCropImage, startY + lastCell.Item2 * height);
+                }
+                else
+                {
+                    var lastLeft = Canvas.GetLeft(_selectedCropImage);
+                    var lastTop = Canvas.GetTop(_selectedCropImage);
+                    Canvas.SetLeft(_selectedCropImage, lastLeft + dx);
+                    Canvas.SetTop(_selectedCropImage, lastTop + dy);
+                    _lastPosition = position;
+                }
 
             }
         }
+
+
     }
 }
